ea85e625964290b0dda82dde54a42a45b322bfb3
Mahfoud-Sa <binsabbah2013@gmail.com>
2024-03-12 03:27:07 +0300

    create_project_strucher_command

--- extension.js
+++ extension.js
@@ -1,29 +1,128 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const fs = require('fs');
+vscode.window.showInformationMessage('Wecome to clean artchiture extenchion');
+function createFolder(folderName){
+	var path=__dirname+folderName;
+	try{
+		if(!fs.existsSync(path)){
+			fs.mkdirSync(path);
+			return 'Done >>>';
+		}
+		else{
+			return 'Folder Exisit >>>';
+		}
+	}catch(err){
+		return err;
+	}
+	
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "myfirstextenchion" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('myfirstextenchion.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('myfirstextenchion.create project strucher', function () {
+		var state='Done >>>';
+		
+		createFolder('/lib/')
+		createFolder('/lib/app')
+		//config folder
+		createFolder('/lib/app/config')
+		createFolder('/lib/app/config/themes')
+		createFolder('/lib/app/config/routes')
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from myFirstExtenchion!');
+		//core folder
+		createFolder('/lib/')
+		createFolder('/lib/app')
+		createFolder('/lib/app/core')
+		createFolder('/lib/app/core/utile')
+		createFolder('/lib/app/core/usecases')
+		createFolder('/lib/app/core/resources')
+		createFolder('/lib/app/core/errors')
+		createFolder('/lib/app/core/widgets')
+
+
+		//function
+		createFolder('/lib/app/features')
+
+		
+		
+	
+	
+	
+
+	
+		vscode.window.showInformationMessage(state);
 	});
+	let disposable_1 = vscode.commands.registerCommand('myfirstextenchion.create project injection container file', function () {
+		//Injection_container file
+	//	fs.open(__dirname+'/app/injection_container.dart','w',function(){});
 
+
+		
+	// 	fs.mkdir(__dirname+'/app/features/featur_name/',function(err){
+	
+	// 		console.log(err)
+	// 	});
+	// 	//data
+	// fs.mkdir(__dirname+'/app/features/featur_name/data/',function(err){
+	
+	// 	console.log(err)
+	// });
+	// fs.mkdir(__dirname+'/app/features/featur_name/data/data_sources',function(err){
+	
+	// 	console.log(err)
+	// });
+	// fs.mkdir(__dirname+'/app/features/featur_name/data/modules',function(err){
+	
+	// 	console.log(err)
+	// });
+	// fs.mkdir(__dirname+'/app/features/featur_name/data/repositories',function(err){
+	
+	// 	console.log(err)
+	// });
+	// //domain
+	// fs.mkdir(__dirname+'/app/features/featur_name/domain/',function(err){
+	
+	// 	console.log(err)
+	// });
+	// fs.mkdir(__dirname+'/app/features/featur_name/domain/entities',function(err){
+	
+	// 	console.log(err)
+	// });
+	// fs.mkdir(__dirname+'/app/features/featur_name/domain/repositories',function(err){
+	
+	// 	console.log(err)
+	// });
+	// fs.mkdir(__dirname+'/app/features/featur_name/domain/usecases',function(err){
+	
+	// 	console.log(err)
+	// });
+	// //presentation
+	// fs.mkdir(__dirname+'/app/features/featur_name/presentation/',function(err){
+	
+	// 	console.log(err)
+	// });
+	// fs.mkdir(__dirname+'/app/features/featur_name/presentation/pages',function(err){
+	
+	// 	console.log(err)
+	// });
+	// fs.mkdir(__dirname+'/app/features/featur_name/presentation/widgets',function(err){
+	
+	// 	console.log(err)
+	// });
+	// fs.mkdir(__dirname+'/app/features/featur_name/presentation/stat_managment',function(err){
+	
+	// 	console.log(err)
+	// });
+		
+
+		vscode.window.showInformationMessage('Done >>>');
+	});
 	context.subscriptions.push(disposable);
 }
 
